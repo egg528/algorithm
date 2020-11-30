afdb065652385209795e576de212901173111822
egg528 <62459196+egg528@users.noreply.github.com>
2020-12-01 00:05:40 +0900

    add Divide and Conquer practice1

--- study/Divide and Conquer/mergeSort.py
+++ study/Divide and Conquer/mergeSort.py
@@ -2,34 +2,29 @@
 list = [2,5,6,8,3,6,5,3,1]
 n = 9
 
-def merge_sort(arr):
-    if len(arr) < 2:
-        return arr
+def mergeSort(list):
+    if len(list) < 2:
+        return list
 
-    mid = len(arr) // 2
-    low_arr = merge_sort(arr[:mid])
+    mid = len(list)//2
+    left_arr = mergeSort(list[:mid])
+    right_arr = mergeSort(list[mid:])
+    return merge(left_arr, right_arr)
 
-    high_arr = merge_sort(arr[mid:])
+def merge(left_arr, right_arr):
+    merged_list = []
 
-    return merge(low_arr, high_arr)
-
-def merge(low_arr, high_arr):
-
-
-    merged_arr =[]
     l = h = 0
-    while l < len(low_arr) and h < len(high_arr):
-        if low_arr[l] < high_arr[h]:
-            merged_arr.append(low_arr[l])
-            l += 1
+    while l < len(left_arr) and h < len(right_arr):
+        if left_arr[l] < right_arr[h]:
+            merged_list.append(left_arr[l])
+            l+=1
         else:
-            merged_arr.append(high_arr[h])
+            merged_list.append(right_arr[h])
             h += 1
-    merged_arr += low_arr[l:]
-    merged_arr += high_arr[h:]
 
+    merged_list += left_arr[l:]
+    merged_list += right_arr[h:]
+    return merged_list
 
-
-    return merged_arr
-
-print(merge_sort(list))
+print(mergeSort(list))
---
+++ study/Divide and Conquer/quickSort.py
@@ -0,0 +1,25 @@
+list = [2,4,1,5,7,8,9,3]
+n = 13
+
+def quickSort(list):
+    if len(list) < 2:
+        return list
+    pivot = partition(list)
+    return quickSort(list[:pivot]) + [list[pivot]] + quickSort(list[pivot+1:])
+
+
+
+
+def partition(list):
+    pivot = list[0]
+    j = 0
+    for i in range(1,len(list)):
+        if list[i] <= pivot:
+            j+=1
+
+            list[j], list[i] = list[i], list[j]
+
+    list[0], list[j] = list[j], list[0]
+    return j
+
+print(quickSort(list))
