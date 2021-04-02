237caad3378996e0a2eb39a3a5581fd765c3b27d
egg528 <62459196+egg528@users.noreply.github.com>
2021-04-02 23:58:54 +0900

    1day 1algo

---
+++ Java/SWEA/P3314.java
@@ -0,0 +1,30 @@
+package SWEA;
+
+
+}class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+
+		Scanner sc = new Scanner(System.in);
+		int T;
+		T=sc.nextInt();
+
+		for(int test_case = 1; test_case <= T; test_case++)
+		{
+		
+			int sum = 0;
+            
+            for(int i = 0; i < 5; i++){
+            	int score = sc.nextInt();
+                if(score <= 40){
+                	sum+=40;
+                 }else{
+                	sum+=score;
+                }
+            }
+            
+            System.out.println("#"+test_case+" "+sum/5);
+		}
+	}
+}
