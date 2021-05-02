e7c37135074c564d26273f064dd7d685b9abfade
egg528 <62459196+egg528@users.noreply.github.com>
2021-05-03 08:32:52 +0900

    1day 1algo

--- Java/SWEA/P4789.java
+++ Java/SWEA/P4789.java
@@ -1,38 +1,59 @@
 package SWEA;
 
-import java.util.Scanner;
-import java.io.FileInputStream;
+import java.util.*;
 
 
 class Solution
 {
+    static int answer;
+    
 	public static void main(String args[]) throws Exception
 	{
 
-		Scanner sc = new Scanner(System.in);
-		int T;
-		T=sc.nextInt();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.valueOf(br.readLine());
 
 
 		for(int test_case = 1; test_case <= T; test_case++)
 		{
-			String audience = sc.next();
+		
+            String[] NL = br.readLine().split(" ");
+            int N = Integer.valueOf(NL[0]);
+            int L = Integer.valueOf(NL[1]);
             
-            int needAud = 0;
-            int nowAud = Integer.valueOf(audience.substring(0, 1));
             
-            for(int i = 1; i < audience.length(); i++){
-            	int need = Integer.valueOf(audience.substring(i, i+1));
+            int[] flavor = new int[N];
+            int[] cal = new int[N];
+            
+            for(int i = 0; i < N; i++){
+                String[] temp = br.readLine().split(" ");
+                flavor[i] = Integer.valueOf(temp[0]);
+                cal[i] = Integer.valueOf(temp[1]);
                 
-                if(nowAud < i){
-                	needAud += i-nowAud;
-                    nowAud += i-nowAud;
-                }
-                nowAud += need;
             }
             
-            System.out.println("#"+test_case+" "+needAud);
-
+            answer = 0;
+            
+            bfs(flavor, cal, 0, 0, 0, L);
+            
+            System.out.println("#"+test_case+" "+answer);
+            
 		}
-	}
+    }
+        public static void bfs(int[] flavor, int[] cal, int now, int flavorSum, int calSum, int restrict){
+        	if(restrict< calSum){
+            	return;
+            }
+            if(now == flavor.length){
+            	if(calSum <= restrict && answer < flavorSum){
+                	answer = flavorSum;
+                }
+                return;
+            }
+            if(answer < flavorSum){
+            	answer = flavorSum;
+            }
+            bfs(flavor, cal, now+1, flavorSum, calSum, restrict);
+            bfs(flavor, cal, now+1, flavorSum+flavor[now], calSum+cal[now], restrict);
+        }
 }
